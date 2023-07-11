166fe8a496c5ee2602c6a795c41bf126b2354ece
Kevin Cabello <keving758@gmail.com>
2023-07-10 21:58:23 -0400

    SpringBoot

---
+++ src/main/java/cl/awakelab/grupo6/Modulo6/HelloController.java
@@ -0,0 +1,16 @@
+package cl.awakelab.grupo6.Modulo6;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hola Mundo";
+    }
+
+
+
+}
---
+++ src/main/java/cl/awakelab/grupo6/Modulo6/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cl.awakelab.grupo6.Modulo6;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Modulo6Application.class);
+	}
+
+}
