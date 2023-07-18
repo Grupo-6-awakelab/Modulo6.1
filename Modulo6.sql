12a7eafa347f862c04d8ef1082cf566323823699
Kevin Cabello <keving758@gmail.com>
2023-07-17 22:21:38 -0400

    Update Modulo6.sql

--- Modulo6.sql
+++ Modulo6.sql
@@ -7,6 +7,19 @@ CREATE TABLE capacitacion (
   detalle TEXT
 );
 
+CREATE TABLE usuario (
+id INT PRIMARY KEY AUTO_INCREMENT,
+nombre varchar(100) NOT NULL,
+apellido varchar (100) NOT NULL,
+direccion varchar (100) NOT NULL
+);
+
+INSERT INTO usuario (nombre, apellido, direccion)
+VALUES ('Usuario 1', 'Apellido 1', 'Direccion 1'),
+('Usuario 2','Apellido 2', 'Direccion 2'),
+('Usuario 3', 'Apelldio 3', 'Direccion 3');
+
+
 INSERT INTO capacitacion(nombre,detalle)
 VALUES('Capacitacion 1', 'Detalle1'),
 ('Capacitacion 2', 'Detalle 2'),
